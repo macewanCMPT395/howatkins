fb58fe397c756b00f7805c3e627c5ee0db6691b6
Eric <eric_watkins_6@hotmail.com>
2015-01-23 13:00:22 -0500

    created css basic and modified the default layout

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,9 +1,20 @@
-<!doctype html>
+<!DOCTYPE HTML PUBLIC>
 <html>
 <head>
-    <meta charset="utf-8">
+
+<link rel="stylesheet" type="text/css" href="/css/basic.css">
 </head>
+
 <body>
-    @yield('content')
+    <div class="title">
+        <h1>CMPT395</h1>
+    </div>    
+    <div id="nav">    
+        <ul>
+            <li><a href="/" id="homenav" >Home</a></li>
+            <li><a href="/" id="loginnav" >Login</a></li>
+            @yield('content')
+        </ul>
+    </div>
 </body>
 </html>
