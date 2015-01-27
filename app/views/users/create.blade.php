cf791a93667501f4233592896dcc6b7ed182c2b8
Eric <eric_watkins_6@hotmail.com>
2015-01-26 21:42:47 -0500

    added an upload function i found to the file, needs work tho it is not complete

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -39,4 +39,9 @@
     <div>{{ Form::submit('Create User') }}</div>
     </fieldset>
 {{ Form::close() }}
+<!--//http://davidwalsh.name/basic-file-uploading-php-->
+<form action="accept-file.php" method="post" enctype="multipart/form-data">
+	Your Photo: <input type="file" name="photo" size="25" />
+	<input type="submit" name="submit" value="Submit" />
+</form>
 @stop
