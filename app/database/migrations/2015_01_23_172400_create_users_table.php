370615e02b7c79cab5198c476039147e84e99e88
Eric <eric_watkins_6@hotmail.com>
2015-01-30 13:41:03 -0500

    Our app now uses sqlite

--- app/database/migrations/2015_01_23_172400_create_users_table.php
+++ app/database/migrations/2015_01_23_172400_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration {
             $table->string('password');
             $table->timestamps();
             $table->rememberToken();
-	    $table->boolean('image');
+	    $table->boolean('image')->default(false);
         });
 	}
 
